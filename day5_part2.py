39bb46e5f7dcafe2c3cbacbe7414677f4a45175f
manos02 <64806766+manos02@users.noreply.github.com>
2025-12-05 20:01:47 +0100

    day 5

---
+++ day5_part2.py
@@ -0,0 +1,32 @@
+ranges = []
+values = []
+
+in_second_block = False
+
+with open("input.txt") as f:
+    for line in f:
+        line = line.strip()
+        if not line:            
+            in_second_block = True
+            continue
+
+        if in_second_block:
+            values.append(int(line))
+        else:
+            # print(line.split("-"))
+            con = lambda a : (int(a[0]),int(a[1]))
+            ranges.append(con(line.split("-")))
+
+
+total = 0
+for v in values:
+    for p in ranges:
+        if v >= p[0] and v <= p[1]:
+            total += 1
+            break
+    # low, high = r.split("-")
+    # print(low, high)
+
+
+
+print(total)
